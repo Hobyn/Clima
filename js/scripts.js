c057de0a8f4fe8a10625b01ff462cb235468bab5
Roberto <30988803+Hobyn@users.noreply.github.com>
2022-09-25 22:54:52 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,50 +13,74 @@ const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
-
+const erro = document.getElementById("teste")
 const weatherContainer = document.querySelector("#weather-data")
 
-const getWeatherData = async (city) =>{
+const getWeatherData = async (city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
 
     const res = await fetch(apiWeatherURL)
     const data = await res.json()
 
+
+
     return data;
+
+
 }
 
 
 const showWeatherData = async (city) => {
+
+
     const data = await getWeatherData(city);
 
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp);
-    descElement.innerText = data.weather[0].description;
-    weatherIconElement.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", apiCountryURL + data.sys.country );
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`
+    if (data.name === undefined) {
+        cityElement.innerText = `Cidade não encontrada`
+        tempElement.innerText = 'erro'
+        descElement.innerText = 'erro'
+        weatherIconElement.innerText = 'erro'
+        countryElement.innerText = 'erro'
+        humidityElement.innerText = 'erro'
+        windElement.innerText = 'erro'
+        weatherContainer= ''
+    } else {
+        cityElement.innerText = `${data.name}, ` + ` ${data.sys.country}`;
+        tempElement.innerText = parseInt(data.main.temp);
+        descElement.innerText = data.weather[0].description;
+        weatherIconElement.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+        humidityElement.innerText = `${data.main.humidity}%`;
+        windElement.innerText = `${data.wind.speed}km/h`
+        weatherContainer.classList.remove("hide")
 
-    weatherContainer.classList.remove ("hide")
+
+        weatherContainer.classList = '';
+
+    }
+
 };
-
-
-
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    const city = cityInput.value;
 
+
+
+    const city = cityInput.value;
     showWeatherData(city);
+
+
+
 })
 
 
-cityInput.addEventListener("keyup", (e) =>{
-if(e.code === "Enter"){
-    const city = e.target.value;
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
 
-    showWeatherData(city);
+        showWeatherData(city);
 
 
-}
+    }
 })
